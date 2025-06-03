3c7978405b268c66380a1292a8d67cc7b1503b7f
Patricia <patricialycacastro@gmail.com>
2025-06-03 11:10:33 +0800

    Added icons to col-skills

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      }
+    });
+  },
+  {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1],
+  }
+);
+const tags = document.querySelectorAll("section");
+tags.forEach((tag) => {
+  observer.observe(tag);
+});
+
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar-mobile");
   sidebar.style.display = "flex";
